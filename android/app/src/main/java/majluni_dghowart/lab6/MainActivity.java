3b08601c653cd1234422b4b36b9b0c2bea94c3fa
Maurice Ajluni <majluni@asu.edu>
2018-11-21 16:26:05 -0700

    Setup second activity, working on switching to web view on button click

--- android/app/src/main/java/majluni_dghowart/lab6/MainActivity.java
+++ android/app/src/main/java/majluni_dghowart/lab6/MainActivity.java
@@ -1,9 +1,12 @@
 package majluni_dghowart.lab6;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,13 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-        WebView browser = findViewById(R.id.webview);
-        browser.setWebViewClient(new WebViewClient());
-        browser.setHorizontalScrollBarEnabled(true);
-        browser.setVerticalScrollBarEnabled(true);
-        browser.setWebContentsDebuggingEnabled(true);
-        browser.getSettings().setJavaScriptEnabled(true);
-        browser.loadUrl(getString(R.string.wvURL));
+    public void onButtonClick(View v) {
+        EditText text = findViewById(R.id.editText);
+        String username = text.getText().toString();
+
+        if (!username.equals("")) {
+            Intent wv = new Intent(this, WebViewActivity.class);
+            startActivity(wv);
+        }
+        else {
+            Button pressed = findViewById(R.id.button);
+            pressed.setVisibility(View.VISIBLE);
+            TextView info = findViewById(R.id.textView2);
+            info.setText(getString(R.string.badUser));
+        }
     }
 }
