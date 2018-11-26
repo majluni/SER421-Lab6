cb358a0044d197b1e85621efd14de0c0182f1b89
Maurice Ajluni <majluni@asu.edu>
2018-11-25 20:46:59 -0700

    Local prject files for my machine

---
+++ android/app/src/main/java/majluni_dghowart/lab6/WebViewActivity.java
@@ -0,0 +1,23 @@
+package majluni_dghowart.lab6;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class WebViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_webview);
+
+        WebView browser = findViewById(R.id.webview);
+        browser.setWebViewClient(new WebViewClient());
+        browser.setHorizontalScrollBarEnabled(true);
+        browser.setVerticalScrollBarEnabled(true);
+        browser.setWebContentsDebuggingEnabled(true);
+        browser.getSettings().setJavaScriptEnabled(true);
+        browser.loadUrl(getString(R.string.wvURL));
+    }
+}
