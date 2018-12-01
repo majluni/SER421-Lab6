80ff873afb6c50e052027d465d8e981ae9b9a84a
ProgKing <dhowarth161@gmail.com>
2018-11-30 20:41:56 -0700

    Added additional comments

--- android/app/src/main/java/majluni_dghowart/lab6/MainActivity.java
+++ android/app/src/main/java/majluni_dghowart/lab6/MainActivity.java
@@ -1,4 +1,4 @@
-package majluni_dghowart.lab6;
+package com.example.dhowa.ser421;
 
 import android.app.PendingIntent;
 import android.app.TaskStackBuilder;
@@ -13,11 +13,12 @@ import android.widget.TextView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-
+//Main Activity that starts the application
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //Main activity layout
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -46,19 +47,24 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    //Button click that takes the user to the next activity (web view)
     public void onButtonClick(View v) {
         EditText text = findViewById(R.id.editText);
         String username = text.getText().toString();
 
+        //Check to see if they entered a username
         if (!username.equals("")) {
+            //Intent for next activity
             Intent wv = new Intent(this, WebViewActivity.class);
             wv.putExtra("name",username);
+            //Used: https://developer.android.com/training/implementing-navigation/temporal#java for back button
             PendingIntent pend = TaskStackBuilder.create(this).addNextIntentWithParentStack(wv).getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
             NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
             builder.setContentIntent(pend);
             startActivity(wv);
         }
         else {
+            //Displays
             Button pressed = findViewById(R.id.button);
             pressed.setVisibility(View.VISIBLE);
             TextView info = findViewById(R.id.textView2);
--- android/app/src/main/java/majluni_dghowart/lab6/WebViewActivity.java
+++ android/app/src/main/java/majluni_dghowart/lab6/WebViewActivity.java
@@ -1,4 +1,4 @@
-package majluni_dghowart.lab6;
+package com.example.dhowa.ser421;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,28 +14,30 @@ public class WebViewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_view);
-
+        //Get name from previous activity
         final String name = getIntent().getStringExtra("name");
-
+        //Enable webview browser contents such as debugging scroll bars etc
         browser = findViewById(R.id.webView);
         browser.setHorizontalScrollBarEnabled(true);
         browser.setVerticalScrollBarEnabled(true);
 
         browser.setWebContentsDebuggingEnabled(true);
 
+        //Create the webChromeClient and enable javascript, URL file access, and DOM storage
         browser.setWebChromeClient(new WebChromeClient());
         WebSettings web = browser.getSettings();
 
         web.setAllowUniversalAccessFromFileURLs(true);
         web.setDomStorageEnabled(true);
         web.setJavaScriptEnabled(true);
+        //Once the page is done loading manipulate the DOM used: https://stackoverflow.com/questions/3149216/how-to-listen-for-a-webview-finishing-loading-a-url by: ian
         browser.setWebViewClient(new WebViewClient(){
             public void onPageFinished(WebView view, String url){
                 browser.evaluateJavascript("changeName(\""+ name+ "\")", null);
                 browser.evaluateJavascript("welcomeUser()",null);
             }
         });
-
+        //Load url and add Javascript interface
         browser.loadUrl("file:///android_asset/html/clue.html");
         browser.addJavascriptInterface(new JSInterface(this), "Android");
     }
